94131692f8a3076a218e274335405ffde1ce2cde
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2024-04-05 15:31:04 +0800

    Buttons

--- rqsplus/src/App.jsx
+++ rqsplus/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 import { Modal } from "./components/Modal";
 import { Table } from "./components/Table";
 import "./App.css";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="App">
       <Table />
-      <Modal />
+      <button className="btn" onClick={() => setModalOpen(true)}>
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+          }}
+        />
+      )}
     </div>
   );
 }
--- rqsplus/src/components/Modal.jsx
+++ rqsplus/src/components/Modal.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 import "./Modal.css";
 
-export const Modal = () => {
+export const Modal = ({ closeModal }) => {
   return (
-    <div className="modal-container">
+    <div
+      className="modal-container"
+      onClick={(e) => {
+        if (e.target.className === "modal-container") closeModal();
+      }}>
       <div className="modal">
         <form>
           <div className="form-group">
