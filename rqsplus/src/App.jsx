eefb6358a2d60bcec8e813330234103e6749561d
kaylee01 <65952228+kaylee01@users.noreply.github.com>
2024-04-05 16:03:41 +0800

    No hard coding

--- rqsplus/src/App.jsx
+++ rqsplus/src/App.jsx
@@ -6,9 +6,16 @@ import "./App.css";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const rows = [
+    { page: "Page 1", description: "this is 1st page", status: "live" },
+    { page: "Page 2", description: "this is 2nd page", status: "draft" },
+    { page: "Page 3", description: "this is 3rd page", status: "error" },
+  ];
+
   return (
     <div className="App">
-      <Table />
+      <Table rows={rows} />
       <button className="btn" onClick={() => setModalOpen(true)}>
         Add
       </button>
--- rqsplus/src/components/Table.jsx
+++ rqsplus/src/components/Table.jsx
@@ -4,58 +4,38 @@ import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 
 import "./Table.css";
 
-export const Table = () => {
+export const Table = ({ rows }) => {
   return (
     <div className="table-wrapper">
       <table className="table">
         <thead>
           <tr>
             <th>Page</th>
-            <th className="expand"> Descirption</th>
+            <th className="expand"> Description</th>
             <th>Status</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Home</td>
-            <td>main page</td>
-            <td>
-              <span className="label label-live">Live</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 2</td>
-            <td>2nd page</td>
-            <td>
-              <span className="label label-draft">Draft</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 3</td>
-            <td>3rd page</td>
-            <td>
-              <span className="label label-error">Error</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
+          {rows.map((row, idx) => {
+            return (
+              <tr key={idx}>
+                <td>{row.page}</td>
+                <td className="expand">{row.description}</td>
+                <td>
+                  <span className={`label label-${row.status}`}>
+                    {row.status}
+                  </span>
+                </td>
+                <td>
+                  <span className="actions">
+                    <BsFillTrashFill className="delete-btn" />
+                    <BsFillPencilFill />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
